d4574e0de961925ae2405a8bd5de4135dd677394
claudiohbsantos <claudiohbsantos@gmail.com>
2021-01-06 00:37:44 -0500

    call setTimeout from window scope

--- src/toggleInfoContent.js
+++ src/toggleInfoContent.js
@@ -14,14 +14,14 @@ export function toggleInfoVisibility() {
   if (+infoContent.style.opacity) {
     infoContent.style.opacity = 0;
     scroller.toTop(0)
-    setTimeout(() => {
+    window.setTimeout(() => {
       infoContent.style.display = 'none';
       document.body.style.overflow = 'hidden';
     }, transitionDuration);
   } else {
     infoContent.style.display = 'block';
     document.body.style.overflow = 'auto';
-    setTimeout(() => {
+    window.setTimeout(() => {
       infoContent.style.opacity = 1;
     }, 0);
   }
